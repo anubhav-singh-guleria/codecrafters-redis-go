741ed0af013f1d8a49ed2bfced5f42051de13ac9
asura69 <you@example.com>
2024-08-01 23:55:52 +0530

    PING

--- app/server.go
+++ app/server.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	
+	"strings"
 	"net"
 	"os"
 )
@@ -16,9 +16,24 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
+	con , err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+
+	buffer := make([]byte, 2048)
+	byte_count , _ := con.Read(buffer)
+
+	command_list := strings.Split(string(buffer[:byte_count]) , "\r\n")
+	// command_list = strings.Split(command_list[0], " ")
+	fmt.Println(command_list[2])
+	if(command_list[2] == "PING"){
+		if(len(command_list[3]) == 0){
+			con.Write([]byte("+PONG\r\n"))
+		}else{
+			con.Write([]byte(strings.Join(command_list[3:], "\r\n")))
+		}
+	}
+
 }
