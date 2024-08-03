ef5d785fbd4b3a71873ffda85a3b9cbea5db4132
asura69 <you@example.com>
2024-08-03 18:13:01 +0530

    multiple ping commands

--- app/server.go
+++ app/server.go
@@ -1,8 +1,9 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"strings"
+	"io"
 	"net"
 	"os"
 )
@@ -10,30 +11,38 @@ import (
 func main() {
 	fmt.Println("Logs from your program will appear here!")
 
-	
-	l, err := net.Listen("tcp", "0.0.0.0:6379")
+	listner, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	con , err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
-
-	buffer := make([]byte, 2048)
-	byte_count , _ := con.Read(buffer)
-
-	command_list := strings.Split(string(buffer[:byte_count]) , "\r\n")
-	// command_list = strings.Split(command_list[0], " ")
-	fmt.Println(command_list[2])
-	if(command_list[2] == "PING"){
-		if(len(command_list[3]) == 0){
-			con.Write([]byte("+PONG\r\n"))
-		}else{
-			con.Write([]byte(strings.Join(command_list[3:], "\r\n")))
+	defer listner.Close()
+	for {
+		fmt.Println("a command requested")
+		con, err := listner.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
 		}
+		go handleClient(con)
 	}
 
+}
+
+func handleClient(conn net.Conn) {
+	// Ensure we close the connection after we're done
+	defer conn.Close()
+
+	for {
+		buf := make([]byte, 1024)
+		_, err := conn.Read(buf)
+		if errors.Is(err, io.EOF) {
+			break
+		}
+		if err != nil {
+			fmt.Println("Error", err)
+			return
+		}
+		conn.Write([]byte("+PONG\r\n"))
+	}
 }
