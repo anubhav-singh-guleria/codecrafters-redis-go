15de435923ae60dadb9e4348ca595782f30d60c4
Anubhav <anubhavsguleria@gmail.com>
2024-08-22 19:25:08 +0530

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -45,6 +45,10 @@ func main() {
 			n, _ = connection.Read(buf)
 			if(n != 0){
 				connection.Write([]byte("*3\r\n$8\r\nREPLCONF\r\n$4\r\ncapa\r\n$6\r\npsync2\r\n"))
+				n, _ = connection.Read(buf)
+				if(n != 0){
+					connection.Write([]byte("*3\r\n$5\r\nPSYNC\r\n$1\r\n?\r\n$2\r\n-1\r\n"))
+				}
 			}
 		}
 	}
